bca1a02d010b5a03491255980127d1b4e774c18a
peter L <peter.l@uvaper.com>
2021-01-09 15:01:09 +0900

    react router study #1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import Profile from "./Profile";
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/profiles/:username" component={Profile} />
+    </div>
+  );
+};
+
+export default App;
